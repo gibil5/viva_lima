32a8bca926813a242e84aa515144bee95b94c544
gibil <gibil@oblique.org>
2014-08-19 14:50:16 -0500

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+
+
+#jr@oblique: 19/9/14
+
+
+class StaticPagesController < ApplicationController
+
+
+# Executes the code and then renders the view
+
+  def home
+
+  end
+
+
+  def help
+  end
+
+end
